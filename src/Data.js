5818ea68c597373d177a6cd3f36d73e0f2621a0e
zhaokaren <karen.zhao@mail.utoronto.ca>
2019-07-13 22:20:29 -0400

    Photo formatting/DUpdated Descriptions

--- src/Data.js
+++ src/Data.js
@@ -8,25 +8,26 @@ import seven from './photos/seven.png';
 import one2 from './photos/Photo2.jpeg';
 
 
-var Data = [{ratings:{"Aesthetic:":5, "Hangout:":2, "Coffee:":4},
+var Data = [{ratings:{"Aesthetic:":4, "Hangout:":5, "Workplace":5 },
             contentId:"good",
             photo: require("./photos/seven.png") ,
             photos:[require("./photos/seven.png")],
             name:"Boxcar Social - Summerhill",
             location:"Yonge and Summerhill",
-            maps:"",
-            blurb:"In the heart of yorkville, this small shop is only good for grab and go coffee. With a beautiful marble counter top, dark interior with bright copper accents, there isn't anything like this in the city."},
+            maps:"https://www.google.com/maps/place/Boxcar+Social/@43.6817068,-79.3916378,15z/data=!4m2!3m1!1s0x0:0x642244564cef5ca7?sa=X&ved=2ahUKEwjR_vntpbPjAhUOWs0KHTLJAkEQ_BIwCnoECA4QCA",
+            blurb:"Right by Summerhill Subway Station, this cute house transformed into coffee shop has character. Interior is decorated with wooden accents, plants and small trinkets gives this shop a homey vibe. This is a go to spot if you are looking for alcohol and coffee all in one go."},
             {
-            ratings:{"Aesthetic:":5, "Workplace:":0, "Hangout:":2, "Coffee:":4},
+            ratings:{"Aesthetic:":5, "Workplace:":0, "Hangout:":2},
             contentId:"drinks",
             photo: require('./photos/eight.jpg'),
             photos:[require('./photos/eight.jpg')],
             name:"Sorry Coffee Co.",
             location:"Bloor and Bay",
-            maps:"https://www.google.com/maps/place/Sorry+Coffee+Co/@43.669797,-79.3917437,15z/data=!4m2!3m1!1s0x0:0xf219ecb45585c9a7?sa=X&ved=2ahUKEwi3qIr9rN_iAhUMTd8KHftyC7sQ_BIwDnoECAsQCA"
+            maps:"https://www.google.com/maps/place/Sorry+Coffee+Co/@43.669797,-79.3917437,15z/data=!4m2!3m1!1s0x0:0xf219ecb45585c9a7?sa=X&ved=2ahUKEwi3qIr9rN_iAhUMTd8KHftyC7sQ_BIwDnoECAsQCA",
+            blurb:"Hidden spot in yorkville, a spot for grab and go coffee. Dark interior with stark white marble counter and copper accents is definitely one of a kind. If you’re in the area, I would recommend grabbing a drink then just sitting outside on the large rock and chill out with your friends."
             },
             {
-            ratings:{"Aesthetic:":4, "Workplace:":3, "Hangout:":2, "Coffee:":4},
+            ratings:{"Aesthetic:":3, "Workplace:":3, "Hangout:":2},
             contentId:"good",
             photo:one,
             photos:[one,one2],
@@ -34,36 +35,36 @@ var Data = [{ratings:{"Aesthetic:":5, "Hangout:":2, "Coffee:":4},
             name:"Strange Love Coffee",
             location:"Queen St. West and Bathurst",
             karen:"This is the spot to hit if you want something quick for on the go. There is a seating workspace, but the sapce itself isnt fully furnished.",
-            blurb:"Off to the side of the city, the location of Strange Love brings a different type of vibe. A middle sized coffeeshop mixed with a tatoo parlour definitely brings something new. "
+            blurb:"Slightly off to the side of the city, the location of Strange Love brings a different kind of vibe. Definitely not your typical coffee shop. This stop is a middle sized coffee shop mixed with a tattoo parlour, something you don’t see everyday. "
             },
             {
-            ratings:{"Aesthetic:":4, "Workplace:":0, "Hangout:":2, "Coffee:":4},
+            ratings:{"Aesthetic:":4, "Workplace:":0, "Hangout:":2},
             contentId:"good",
             photo:require('./photos/one.jpg'),
             photos:[two],
             maps:"https://www.google.com/maps/place/A-OK+CAFE/@43.6703401,-79.3882622,15z/data=!4m2!3m1!1s0x0:0x1f0e8e0a244932dd?sa=X&ved=2ahUKEwiAlt2DjsziAhUFEawKHbpGBNYQ_BIwCnoECA4QCA",
             name:"A-OK",
             location:"Bloor and Yonge",
-            blurb:"A tiny little shop connected to Aritzia, for a grab and go coffee."
+            blurb:"Connected to the newly renovated Aritzia on Bloor, this cute littåle shop is a gem. Beautiful stone interior with different textures. Only good for grab and go coffee." 
             },
             {
-            ratings:{"Aesthetic:":5, "Workplace:":4, "Hangout:":4, "Coffee:":4},
+            ratings:{"Aesthetic:":5, "Workplace:":4, "Hangout:":4},
             contentId:"drinks",
             photo:require('./photos/four.png'),
             photos:[three],
             maps:"https://www.google.com/maps/place/Tokyo+Smoke/@43.6467568,-79.4093899,17z/data=!3m1!4b1!4m5!3m4!1s0x882b34e14accc7fb:0x46b4239ae0014ed5!8m2!3d43.6467529!4d-79.4072012",
             name:"Tokyo Smoke",
             location:"Queen St. West and Bathurst",
-            blurb:"Off to the side of the city, the location of Strange Love brings a different type of vibe A middle sized coffeeshop mixed with a tatoo parlour definitely brings something new."
+            blurb:"A smoke shop and coffee shop all in one what else would you ask for? Tucked in the side of the city, this is definitely one of my favourites that I have yet to come back to. Simple interior with dark accents and marble tables is such a mood. You also don’t have to pay extra for alternative milks !! (I think)"
             },
             {
-                ratings:{"Aesthetic:":4, "Workplace:":5, "Hangout:":4, "Coffee:":0},
+                ratings:{"Aesthetic:":4, "Workplace:":5, "Hangout:":4},
                 photo:four,
                 photos:[four],
                 maps:"https://www.google.com/maps/place/ICHA+TEA/@43.6514215,-79.3992512,17z/data=!3m1!4b1!4m5!3m4!1s0x882b34c4a403ffff:0x4056d5bbc0d5dc3c!8m2!3d43.6514176!4d-79.3970625",
                 name:"Icha Tea",
                 location:"Spadina and Dundas",
-                blurb:"Off to the side of the city, the location of Strange Love brings a different type of vibe. A middle sized coffeeshop mixed with a tatoo parlour definitely brings something new."
+                blurb:"Tea shop with decor that is reminiscent of asia. Lots of old school furniture, and decor that reminds me of ancient china, definitely a spot you have to check out if you are around."
             },
             {
                 ratings:{"Aesthetic:":4, "Workplace:":3, "Hangout:":2, "Coffee:":4},
